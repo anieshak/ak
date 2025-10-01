c13b73a7e6fb4d44bdc03cf83cda549f1714c4ec
ImArvindRaj <rajyarvind@gmail.com>
2025-10-01 15:08:37 +0530

    Styles Changes & Icon added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="min-h-screen md:overflow-hidden flex flex-col items-center justify-between px-4 py-6 bg-[#00000030]">
-      <main className="w-full max-w-4xl">
-        <section className="w-full">
-          <ProfileCard />
-        </section>
+    <div className="min-h-screen md:overflow-hidden flex flex-col py-4 justify-center bg-[#00000030]">
+      <main className="mx-auto 2xl:max-w-5xl max-w-[65%]  px-4 sm:px-6 ">
+        <ProfileCard />
       </main>
       <Footer />
     </div>
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -3,10 +3,9 @@ import profileImg from "../assets/aniesh-profile.jpg";
 
 export default function ProfileCard() {
   return (
-    <div className="bg-white shadow-xl">
-      <div className="flex flex-col md:flex-row gap-4 items-center">
+      <div className="bg-white shadow-xl flex flex-col md:flex-row gap-4 items-center">
         {/* Left: Image Section */}
-        <div className="md:w-1/2 flex-shrink-0">
+        <div className="md:w-1/2 ">
           <div className="relative overflow-hidden bg-gray-100 shadow-md">
             <img
               src={profileImg}
@@ -19,10 +18,10 @@ export default function ProfileCard() {
         {/* Right: Content Section */}
         <div className="flex-1 flex flex-col justify-center p-4">
           <div>
-            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4 text-black italic">
+            <h1 className="text-3xl lg:text-4xl font-bold mb-4 text-black italic">
               Aniesh Kumar
             </h1>
-            <p className="text-base sm:text-lg text-black italic mb-8">
+            <p className="text-base text-lg text-black italic mb-8">
               Hello there! <br />I am a{" "}
               <span>Customer Success Manager</span>{" "}
               passionate about Technology, Security & using tech to make this
@@ -30,12 +29,11 @@ export default function ProfileCard() {
             </p>
 
             {/* Social Links */}
-            <div className="w-full sm:max-w-md">
+            <div className="w-full">
               <SocialLinks />
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
