9f7ea0d9ec2b08fd667411d69b70d6a76c3286e9
Aniesh <smaccer7@gmail.com>
2026-02-16 02:28:26 +0530

    Make website mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SocialLinks from "./components/SocialLinks";
 export default function App() {
   return (
     <Background>
-      <div className="min-h-screen relative overflow-hidden flex flex-col md:pt-[1.6rem] items-center justify-center">
+              <div className="min-h-screen relative overflow-x-hidden flex flex-col md:pt-[1.6rem] items-center justify-start py-6 md:justify-center md:py-0">
         <main className="md:w-[65rem] sm:w-full max-w-full px-4 md:px-6">
           <ProfileCard />
         </main>
